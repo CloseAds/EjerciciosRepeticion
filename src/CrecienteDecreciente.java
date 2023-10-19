3909a1335a1df4df53e36e435257b5ee336c9e78
CloseAds <c7336188@gmail.com>
2023-10-19 10:20:16 +0200

    EjerciciosRepeticion.iml

--- src/CrecienteDecreciente.java
+++ src/CrecienteDecreciente.java
@@ -1,2 +1,32 @@
+import java.util.Scanner;
+
 public class CrecienteDecreciente {
+
+    public static void main(String[] args) {
+
+        System.out.println(" Introduce la secuencia de numeros ");
+
+        Scanner s = new Scanner(System.in);
+        int numeros = 0;
+
+        do {
+            numeros = s.nextInt();
+
+            if (numeros == numeros++) {
+                System.out.println(" Es creciente ");
+            } else if (numeros == numeros--) {
+                System.out.println(" Es decreciente ");
+            } else if ( numeros < 0){
+                System.out.println(" No es ni creciente ni decreciente ");
+
+            }
+        } while ( numeros < 0);
+        numeros = s.nextInt();
+
+        System.out.println(" No se han introducido numeros ");
+
+    }
+
 }
+
+
---
+++ src/RellenadoConPuntos.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class RellenadoConPuntos {
+
+    public  static void main (String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+
+
+    }
+
+}
